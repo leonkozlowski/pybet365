c00c30f38f15d9c724da2c4df6b9df33660a50b5
Leon Kozlowski <leonkozlowski@gmail.com>
2020-07-30 18:39:11 -0400

    Missed comma

--- pybet365/__init__.py
+++ pybet365/__init__.py
@@ -1,9 +1,9 @@
 """Top-level package for pybet365."""
 from .client import (
-    Bet365
+    Bet365,
     Bet365SportId
 )
 
 __author__ = """Leon Kozlowski"""
 __email__ = 'leonkozlowski@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
